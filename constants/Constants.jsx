85eac28a589f9fc384abaa19f36345623f9982cb
himfirstmedia <apps.himfirstmedia@gmail.com>
2025-08-05 18:03:38 +0300

    Update for builds

--- constants/Constants.jsx
+++ constants/Constants.jsx
@@ -1,4 +1,5 @@
 // export const BASE_URL = "http://localhost:5000/api/v1";
 export const BASE_URL = "https://paradise-backend-production.up.railway.app/api/v1";
+export const EXPO_PUBLIC_BASE_URL = "https://paradise-backend-production.up.railway.app/api/v1";
 
  
